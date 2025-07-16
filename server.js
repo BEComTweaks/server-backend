6385113afa0cbdc929f76339947e709234ca9c4c
yzabeast1 <yzabeast1@gmail.com>
2025-07-15 19:04:55 -0500

    enable update and checkOnline function when testing without needing https

--- server.js
+++ server.js
@@ -101,11 +101,22 @@ httpApp.get("/downloadTotals", (req, res) => {
 });
 
 httpApp.post("/update", (req, res) => {
-  res.send("Lazy ass, just do it yourself");
-  console.log(
-    "Hey lazy ass, over here, just press `Ctrl + C` then `git pull`, why the extra steps?",
-  );
+  if (process.argv.includes("--dev")) {
+    updateServer(req, res);
+  }
+  else {
+    res.send("Lazy ass, just do it yourself");
+    console.log(
+      "Hey lazy ass, over here, just press `Ctrl + C` then `git pull`, why the extra steps?",
+    );
+  }
 });
+
+if (process.argv.includes("--dev")) {
+  httpApp.get("/checkOnline", (req, res) => {
+    checkOnline(req, res);
+  });
+}
 
 httpApp.get("/ping", (req, res) => {
   res.send("pong?");
@@ -135,108 +146,11 @@ if (httpsApp) {
   });
 
   httpsApp.post("/update", (req, res) => {
-    const key = req.query.key;
-    if (!key) {
-      res.send("You need a key to update the server.");
-    }
-    if (!filesystem.existsSync(secretStuffPath)) {
-      const newkey = uuidv4();
-      const secretStuff = { key: newkey };
-      dumpJson(secretStuffPath, secretStuff);
-      return res
-        .status(500)
-        .send("Secret stuff file not found. Made a new one.");
-    }
-    const secretStuff = loadJson(secretStuffPath);
-    const storedkey = secretStuff.key;
-    if (key === storedkey) {
-      try {
-        console.log("Pulling from git...");
-        const gitPullOutput = execSync("git pull --rebase").toString();
-        console.log("Pulled from git.");
-        console.log("Updating Submodules...");
-        const gitSubmoduleOutput = execSync("git submodule update").toString();
-        console.log("Updated Submodules");
-        const blue = "\x1b[34m";
-        const gray = "\x1b[90m";
-        const reset = "\x1b[0m";
-        const formattedResponse = `
-        ${blue}Update Successful${reset}
-        ${blue}Git Pull Output:${reset}
-        ${gray}${gitPullOutput}${reset}
-        ${blue}Submodule Update Output:${reset}
-        ${gray}${gitSubmoduleOutput}${reset}
-        Do a GET /checkOnline to see the changes.
-        `;
-        return res.status(200).send(formattedResponse);
-      } catch (error) {
-        const red = "\x1b[31m";
-        const gray = "\x1b[90m";
-        const reset = "\x1b[0m";
-        console.error("Error during git operation:", error);
-        const errorResponse = `
-        ${red}Error${reset}
-        There was an error pulling or updating submodules.
-        ${gray}${error.toString()}${reset}
-        `;
-        return res.status(500).send(errorResponse);
-      }
-    } else {
-      res.send("Wrong key!");
-    }
+    updateServer(req, res);
   });
 
   httpsApp.get("/checkOnline", (req, res) => {
-    try {
-      const gitLogOutput = execSync("git log -1 --format=short").toString();
-      const gitSubmoduleOutput = execSync("git submodule status").toString();
-      const colorizeGitLog = (log) => {
-        return log
-          .replace(
-            /commit\s([a-f0-9]+)/,
-            '<span style="color:#f14e32">commit $1</span>',
-          ) // Commit hash
-          .replace(
-            /Author:\s(.+)/,
-            '<span style="color:#1f8ee2">Author: $1</span>',
-          ) // Author
-          .replace(
-            /Date:\s+(.+)/,
-            '<span style="color:#9c9c9c">Date: $1</span>',
-          );
-      };
-      const colorizeSubmoduleStatus = (status) => {
-        return status.replace(
-          /([+\-0-9a-f]{40})\s([a-zA-Z0-9\-\/]+)/g,
-          '<span style="color:#f14e32">$1</span> <span style="color:#1f8ee2">$2</span>',
-        );
-      };
-      const formattedLog = colorizeGitLog(gitLogOutput);
-      const formattedSubmoduleStatus =
-        colorizeSubmoduleStatus(gitSubmoduleOutput);
-      res.send(`
-        <h1>Server is online</h1>
-        <h3>Local Repo Status</h3>
-        <pre>${formattedLog}</pre>
-        <h3>Submodule Status</h3>
-        <pre>${formattedSubmoduleStatus}</pre>
-        <style>
-          @import url("https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap");
-          body { font-family: "Pixelify Sans"; }
-          pre { font-family: "Pixelify Sans"; background-color: #1e1e1e; padding: 10px; color: #ddd; border-radius: 5px; }
-        </style>
-      `);
-    } catch (error) {
-      console.error("Error during git commands:", error);
-      res.status(500).send(`
-        <h1>Error</h1>
-        <p>There was an error retrieving git information.</p>
-        <pre>${error.toString()}</pre>
-        <style>@import url("https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap");
-        body { font-family:"Pixelify Sans" }
-        pre { font-family: "Pixelify Sans"; background-color: #1e1e1e; padding: 10px; color: #ddd; border-radius: 5px; }</style>
-      `);
-    }
+    checkOnline(req, res);
   });
 
   httpsApp.get("/ping", (req, res) => {
@@ -263,5 +177,109 @@ function downloadTotals(req, res) {
     res.send(
       `There is no such file called downloadTotals${type}.json at the root directory`,
     );
+  }
+}
+function updateServer(req, res) {
+  const key = req.query.key;
+  if (!key) {
+    res.send("You need a key to update the server.");
+  }
+  if (!filesystem.existsSync(secretStuffPath)) {
+    const newkey = uuidv4();
+    const secretStuff = { key: newkey };
+    dumpJson(secretStuffPath, secretStuff);
+    return res
+      .status(500)
+      .send("Secret stuff file not found. Made a new one.");
+  }
+  const secretStuff = loadJson(secretStuffPath);
+  const storedkey = secretStuff.key;
+  if (key === storedkey) {
+    try {
+      console.log("Pulling from git...");
+      const gitPullOutput = execSync("git pull --rebase").toString();
+      console.log("Pulled from git.");
+      console.log("Updating Submodules...");
+      const gitSubmoduleOutput = execSync("git submodule update").toString();
+      console.log("Updated Submodules");
+      const blue = "\x1b[34m";
+      const gray = "\x1b[90m";
+      const reset = "\x1b[0m";
+      const formattedResponse = `
+        ${blue}Update Successful${reset}
+        ${blue}Git Pull Output:${reset}
+        ${gray}${gitPullOutput}${reset}
+        ${blue}Submodule Update Output:${reset}
+        ${gray}${gitSubmoduleOutput}${reset}
+        Do a GET /checkOnline to see the changes.
+        `;
+      return res.status(200).send(formattedResponse);
+    } catch (error) {
+      const red = "\x1b[31m";
+      const gray = "\x1b[90m";
+      const reset = "\x1b[0m";
+      console.error("Error during git operation:", error);
+      const errorResponse = `
+        ${red}Error${reset}
+        There was an error pulling or updating submodules.
+        ${gray}${error.toString()}${reset}
+        `;
+      return res.status(500).send(errorResponse);
+    }
+  } else {
+    res.send("Wrong key!");
+  }
+
+}
+function checkOnline(req, res) {
+  try {
+    const gitLogOutput = execSync("git log -1 --format=short").toString();
+    const gitSubmoduleOutput = execSync("git submodule status").toString();
+    const colorizeGitLog = (log) => {
+      return log
+        .replace(
+          /commit\s([a-f0-9]+)/,
+          '<span style="color:#f14e32">commit $1</span>',
+        ) // Commit hash
+        .replace(
+          /Author:\s(.+)/,
+          '<span style="color:#1f8ee2">Author: $1</span>',
+        ) // Author
+        .replace(
+          /Date:\s+(.+)/,
+          '<span style="color:#9c9c9c">Date: $1</span>',
+        );
+    };
+    const colorizeSubmoduleStatus = (status) => {
+      return status.replace(
+        /([+\-0-9a-f]{40})\s([a-zA-Z0-9\-\/]+)/g,
+        '<span style="color:#f14e32">$1</span> <span style="color:#1f8ee2">$2</span>',
+      );
+    };
+    const formattedLog = colorizeGitLog(gitLogOutput);
+    const formattedSubmoduleStatus =
+      colorizeSubmoduleStatus(gitSubmoduleOutput);
+    res.send(`
+        <h1>Server is online</h1>
+        <h3>Local Repo Status</h3>
+        <pre>${formattedLog}</pre>
+        <h3>Submodule Status</h3>
+        <pre>${formattedSubmoduleStatus}</pre>
+        <style>
+          @import url("https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap");
+          body { font-family: "Pixelify Sans"; }
+          pre { font-family: "Pixelify Sans"; background-color: #1e1e1e; padding: 10px; color: #ddd; border-radius: 5px; }
+        </style>
+      `);
+  } catch (error) {
+    console.error("Error during git commands:", error);
+    res.status(500).send(`
+        <h1>Error</h1>
+        <p>There was an error retrieving git information.</p>
+        <pre>${error.toString()}</pre>
+        <style>@import url("https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap");
+        body { font-family:"Pixelify Sans" }
+        pre { font-family: "Pixelify Sans"; background-color: #1e1e1e; padding: 10px; color: #ddd; border-radius: 5px; }</style>
+      `);
   }
 }
